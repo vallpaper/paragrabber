6229cb465319e5d6f8be4381156cb20db5e83dfd
Ondřej Cvacho <cvachond@fit.cvut.cz>
2016-08-28 18:47:01 +0200

    Project: Simple argument parser

--- src/main.cpp
+++ src/main.cpp
@@ -7,25 +7,64 @@
 #include "hash.h"
 
 #include <iostream>
-#include <fstream>
+#include <unistd.h>
+#include <sstream>
 
 void usage(void)
 {
-    std::cout << "./grabber [-u URL] [-d DIR]" << std::endl
-                << "-u URL\tfile to parse (default: meetangee.com)" << std::endl
-                << "-d DIR\tdownload directory (default: ./data)" << std::endl
-                // << "\t-r\trecursive"
+    std::cout << "./grabber [-u URL] [-d DIR] [-t NUM]" << "\n"
+                << "-u URL\tfile to parse (default: meetangee.com)" << "\n"
+                << "-d DIR\tdownload directory (default: ./data)" << "\n"
+                << "-t NUM\tnumber of threads to run" << "\n"
                 << std::endl;
 }
 
-int main(int argc, const char * argv[])
+// Very simple function to parse the input
+//
+Config parse_input(int argc, char * argv[])
 {
-    // TODO: input parse to config modification
-    Config config;
-    Adler32 adler32;
+    std::string url = FILE_URL;
+    std::string dir = DOWNLOAD_DIR;
+    unsigned int threads = THREADS;
 
+    int c;
+    while ((c = getopt(argc, argv, "u:d:t:")) != -1)
+    {
+        switch (c)
+        {
+            case 'u':
+                url = optarg;
+                break;
+            case 'd':
+                dir = optarg;
+                break;
+            case 't':
+            {
+                std::istringstream i(optarg);
+                i >> threads;
+                break;
+            }
+            case '?':
+                std::cout << "Unsupported argument or argument syntax\n";
+                usage();
+                exit(1);
+                break;
+            default:
+                std::cout << "All undefined arguments will be set default value\n";
+                break;
+        }
+    }
+
+    return Config(url, dir, threads);
+}
+
+int main(int argc, char * argv[])
+{
+    Config config = parse_input(argc, argv);
+    Adler32 adler32;
     Grabber grabber(config, adler32);
 
+    // Where all the magic happens
     grabber.run();
 
     return 0;
